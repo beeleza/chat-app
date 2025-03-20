2a008b985b37fbf432ea12453f59e0cc8f64cc3d
beeleza <higorbeleza.dev@gmail.com>
2025-03-20 12:19:19 -0300

    route to check if the user is logged in

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+
+router.get("/check", protectRoute, checkAuth);
 
 router.post("/signup", signup);
 router.post("/login", login);
